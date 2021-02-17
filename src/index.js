f083eb48a137eec1eb39666cf2aad1d4e42f72a3
Konstantin <shuvalov.2021@list.ru>
2021-02-17 19:39:29 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined){
+    return 0;
+  }else if (array.length === 0){
+    return 0;
+  } else {
+    let newArr = array.sort(function (a,b) {
+      return a - b;
+    })
+  return newArr[0];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
-}
+  if(array === undefined){
+    return 0;
+  }else if (array.length === 0){
+    return 0;
+  }else {
+     let newArr = array.sort(function(a,b){
+     return a - b;
+})
+  return newArr[newArr.length - 1];
+  }
+}  
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined){
+    return 0;
+  }else if (array.length === 0){
+    return 0;
+  } else {
+    let number = 0;
+  for(let i = 0; i < array.length; i++){
+    number += array[i];
+  }
+  return number/array.length;
+}
 }
